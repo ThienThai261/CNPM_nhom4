77fd6483d2237eedb888d0780b521b8a381e2e3b
quocthai2612003 <21130502@st.hcmuaf.edu.vn>
2024-05-28 11:12:44 +0700

    update cart controller
    bây giờ sẽ có ngăn chặn trong cart nếu khách hàng không đủ điều kiện mua hàng

--- WebProject/src/main/java/Controller/AddToCartServlet.java
+++ WebProject/src/main/java/Controller/AddToCartServlet.java
@@ -17,17 +17,13 @@ public class AddToCartServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("AddToCartServlet doPost method is called");
-
         HttpSession session = req.getSession();
         ShoppingCart cart = (ShoppingCart) session.getAttribute("cart");
         if (cart == null) cart = new ShoppingCart();
-        System.out.println("Entering AddToCartServlet");
         String maSp = req.getParameter("masanpham");
-        System.out.println("masanpham parameter: " + maSp);
         if (maSp != null && !maSp.isEmpty()) {
             cart.add(maSp);
             session.setAttribute("cart", cart);
-            System.out.println("add success");
         }
         resp.sendRedirect("home");
         System.out.println(cart.getSize());
--- WebProject/src/main/java/Controller/CartServlet.java
+++ WebProject/src/main/java/Controller/CartServlet.java
@@ -19,11 +19,18 @@ public class CartServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         ShoppingCart gioHang = (ShoppingCart) req.getSession().getAttribute("cart");
         ProductService ps = new ProductService();
+        String errorMessage = req.getParameter("errorMessage");
+        String message = req.getParameter("message");
+        String deletedProductId = req.getParameter("deletedProductId");
+
         if (gioHang == null) {
             gioHang = new ShoppingCart();
             req.getSession().setAttribute("cart", gioHang);
         }
         Map<String, String> listImagesThumbnail = ps.selectImageThumbnail();
+        req.setAttribute("errorMessage", errorMessage);
+        req.setAttribute("message", message);
+        req.setAttribute("deletedProductId", deletedProductId);
         List<CartItems> danhSachSanPham = gioHang.getDanhSachSanPham();
         req.getSession().setAttribute("list-sp", danhSachSanPham);
         req.setAttribute("listImagesThumbnail", listImagesThumbnail);
@@ -33,5 +40,8 @@ public class CartServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        }
+
+
     }
-}
+
