5ff98a47d3a2ace9dbc96a48d3ab27433eefe872
hongphuclong <hongphuclongdl2003@gmail.com>
2024-05-29 23:38:10 +0700

    merge branch

--- WebProject/src/main/java/Service/FeedbackAndRatingService.java
+++ WebProject/src/main/java/Service/FeedbackAndRatingService.java
@@ -1,33 +1,33 @@
-package Service;
-
-import DAO.FeedbackDAO;
-import DAO.RatingDAO;
-import Model.Comment;
-
-import java.util.List;
-
-public class FeedbackAndRatingService {
-    private static FeedbackAndRatingService instance;
-
-    public static FeedbackAndRatingService getInstance() {
-        if (instance == null) instance = new FeedbackAndRatingService();
-
-        return instance;
-    }
-
-    public int saveCommentFeedback(String content, String productId, int idAccount) {
-        return FeedbackDAO.saveCommentFeedback(content, productId, idAccount);
-    }
-
-    public List<Comment> getCommentsByProductId(String productId) {
-        return FeedbackDAO.getCommentsByProductId(productId);
-    }
-
-    public int saveRating(int rating, int idAccount, String productId) {
-        return RatingDAO.saveRating(rating, idAccount, productId);
-    }
-
-    public int getTotalNumberOfComments() {
-        return FeedbackDAO.getTotalNumberOfComments();
-    }
-}
+//package Service;
+//
+//import DAO.FeedbackDAO;
+//import DAO.RatingDAO;
+//import Model.Comment;
+//
+//import java.util.List;
+//
+//public class FeedbackAndRatingService {
+//    private static FeedbackAndRatingService instance;
+//
+//    public static FeedbackAndRatingService getInstance() {
+//        if (instance == null) instance = new FeedbackAndRatingService();
+//
+//        return instance;
+//    }
+//
+//    public int saveCommentFeedback(String content, String productId, int idAccount) {
+//        return FeedbackDAO.saveCommentFeedback(content, productId, idAccount);
+//    }
+//
+//    public List<Comment> getCommentsByProductId(String productId) {
+//        return FeedbackDAO.getCommentsByProductId(productId);
+//    }
+//
+//    public int saveRating(int rating, int idAccount, String productId) {
+//        return RatingDAO.saveRating(rating, idAccount, productId);
+//    }
+//
+//    public int getTotalNumberOfComments() {
+//        return FeedbackDAO.getTotalNumberOfComments();
+//    }
+//}
