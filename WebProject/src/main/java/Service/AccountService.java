c57eb6d8801bb4e3e1e6b3dde9d246e673059282
cubi458 <cubi458@gmail.com>
2024-05-29 20:49:22 +0700

    Cập nhật phân trang, quản lý account và liên hệ

--- WebProject/src/main/java/Service/AccountService.java
+++ WebProject/src/main/java/Service/AccountService.java
@@ -1,3 +1,4 @@
+
 package Service;
 
 import DAO.AccountDAO;
@@ -5,6 +6,7 @@ import Model.Account;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -20,9 +22,18 @@ public class AccountService {
         return AccountDAO.accountByUsername(username);
     }
 
+    public Account getAccountByAccountId(int accountId) {
+        return AccountDAO.getAccountByAccountId(accountId);
+    }
+
     public Account accountByUsernameAndEmail(String username, String email) {
         return AccountDAO.accountByUsernameAndEmail(username, email);
     }
+
+    public int getRoleByAccountId(int accountId) {
+        return AccountDAO.getRoleByAccountId(accountId);
+    }
+
         // Chức năng đăng nhập
     public Account checkLogin(String username, String password) {
         Account account = AccountDAO.accountByUsername(username);
@@ -38,11 +49,25 @@ public class AccountService {
     public boolean isLoginSuccess(Account account) {
         return account.getStatus() == 1;
     }
-        // Chức năng đăng ký
+    // Chức năng đăng ký
     public boolean isPhoneValid(String phone) {
         String regex = "^0[0-9]{9}$";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(phone);
+        return matcher.matches();
+    }
+
+    public boolean isEmail(String email) {
+        String regex = "^[\\w\\-.]+@([\\w\\-]+\\.)+[\\w\\-]{2,4}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+    public boolean checkValidatePassword(String password) {
+        String regex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+.=!])(?!.*\\s).{8,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(password);
         return matcher.matches();
     }
 
@@ -78,9 +103,18 @@ public class AccountService {
     public Account isVerifyEmailSuccess(String code) {
         return AccountDAO.isVerifyEmailSuccess(code);
     }
+    public boolean isVerificationCodeValid(String code) {
+        Account account = AccountDAO.isVerifyEmailSuccess(code);
+        return account != null; // Return true if account is found (valid code)
+    }
+
 
     public int createAccount(String username, String password, String email, String fullname, String number_phone, int status) {
         return AccountDAO.createAccount(username, password, email, fullname, number_phone, status);
+    }
+
+    public int createAccountWithGoogleAndFacebook(String username, String email, String fullname) {
+        return AccountDAO.createAccountWithGoogleAndFacebook(username, email, fullname);
     }
 
     public int deleteAccount(String username, String email) {
@@ -120,5 +154,9 @@ public class AccountService {
 
     public boolean updateUserInfo(String username, String newFullname) {
         return AccountDAO.updateUserInfo(username, newFullname);
+    }
+
+    public boolean isAccountExist(String email) {
+        return !AccountDAO.isAccountExist(email);
     }
 }
